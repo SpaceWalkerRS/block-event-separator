76063cc5b4d5a92dcc0a627e68def974faa5b0c4
Space Walker <spacedoesrs@gmail.com>
2022-03-22 21:27:40 +0100

    better fix for blinking pistons

--- src/main/java/block/event/separator/mixin/client/PistonHeadRendererMixin.java
+++ src/main/java/block/event/separator/mixin/client/PistonHeadRendererMixin.java
@@ -9,6 +9,7 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 
 import net.minecraft.client.renderer.blockentity.PistonHeadRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.piston.PistonHeadBlock;
 import net.minecraft.world.level.block.piston.PistonMovingBlockEntity;
@@ -23,6 +24,22 @@ import net.minecraft.world.level.block.state.BlockState;
 public abstract class PistonHeadRendererMixin {
 
 	@Shadow protected abstract boolean renderBlock(BlockPos blockPos, BlockState blockState, BufferBuilder bufferBuilder, Level level, boolean bl);
+
+	@Redirect(
+		method = "render",
+		require = -1,
+		at = @At(
+			value = "INVOKE",
+			ordinal = 0,
+			target = "Lnet/minecraft/world/level/block/piston/PistonMovingBlockEntity;getProgress(F)F"
+		)
+	)
+	private float fixInvisibleMovingBlocks(PistonMovingBlockEntity blockEntity, float partialTick) {
+		// Moving blocks do not render if the progress is 1.0F. To 'fix'
+		// this we clamp the progress to 0.99F for the check.
+		float progress = blockEntity.getProgress(partialTick);
+		return Mth.clamp(progress, 0.0F, 0.99F);
+	}
 
 	@Redirect(
 		method = "render",
