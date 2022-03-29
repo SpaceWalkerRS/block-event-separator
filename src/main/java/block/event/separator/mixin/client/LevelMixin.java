25100375c99892d3ff2df32aeac9a17a74456da1
Space Walker <spacedoesrs@gmail.com>
2022-03-29 17:15:35 +0200

    fix: entities aren't ticked client side during tick freeze

--- src/main/java/block/event/separator/mixin/client/LevelMixin.java
+++ src/main/java/block/event/separator/mixin/client/LevelMixin.java
@@ -1,26 +1,29 @@
 package block.event.separator.mixin.client;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import block.event.separator.BlockEventCounters;
 
-import net.minecraft.client.multiplayer.MultiPlayerLevel;
+import net.minecraft.world.level.Level;
 
-@Mixin(MultiPlayerLevel.class)
-public abstract class MultiPlayerLevelMixin {
+@Mixin(Level.class)
+public class LevelMixin {
+
+	@Shadow private boolean isClientSide() { return false; }
 
 	@Inject(
-		method = "tickEntities",
+		method = "tickBlockEntities",
 		cancellable = true,
 		at = @At(
 			value = "HEAD"
 		)
 	)
 	private void cancelTick(CallbackInfo ci) {
-		if (BlockEventCounters.frozen) {
+		if (isClientSide() && BlockEventCounters.frozen) {
 			ci.cancel();
 		}
 	}
