0b7a184d014f0eacc44ed1cd01efbd5cea40c06a
Space Walker <spacedoesrs@gmail.com>
2022-04-11 19:27:09 +0200

    fix: client does not tick properly when the game is paused

--- src/main/java/block/event/separator/mixin/client/MinecraftMixin.java
+++ src/main/java/block/event/separator/mixin/client/MinecraftMixin.java
@@ -89,6 +89,8 @@ public class MinecraftMixin implements IMinecraft {
 				Counters.ticks++;
 				ticksThisFrame_bes++;
 			}
+		} else {
+			ticksThisFrame_bes = ticksThisFrame;
 		}
 	}
 
@@ -127,8 +129,6 @@ public class MinecraftMixin implements IMinecraft {
 		)
 	)
 	private void cancelTick(CallbackInfo ci) {
-		Counters.frozen = (ticksThisFrame_bes == 0) && serverFrozen_bes;
-
 		if (ticksThisFrame_bes > 0) {
 			ticksThisFrame_bes--;
 		} else if (!serverFrozen_bes) {
