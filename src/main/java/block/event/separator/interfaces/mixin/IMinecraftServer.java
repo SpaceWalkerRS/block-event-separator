b0bbf330015a9d8384870fb86a4432e3ee1049c2
Space Walker <spacewalkerrs@gmail.com>
2022-07-21 14:00:18 +0200

    add handshake to fix unknown packet spam on vanilla clients

--- src/main/java/block/event/separator/interfaces/mixin/IMinecraftServer.java
+++ src/main/java/block/event/separator/interfaces/mixin/IMinecraftServer.java
@@ -1,11 +1,21 @@
 package block.event.separator.interfaces.mixin;
 
+import net.minecraft.server.level.ServerPlayer;
+
 public interface IMinecraftServer {
 
 	default boolean isPaused() {
 		return false;
 	}
 
+	public void onPlayerJoin_bes(ServerPlayer player);
+
+	public void onPlayerLeave_bes(ServerPlayer player);
+
 	public void postBlockEvents_bes();
+
+	public void onHandshake_bes(ServerPlayer player, String modVersion);
+
+	public boolean isBesClient(ServerPlayer player);
 
 }
