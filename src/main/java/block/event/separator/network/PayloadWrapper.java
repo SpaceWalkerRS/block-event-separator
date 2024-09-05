14cc52a613a1732c24abf0d8ee46f194c6350af7
Space Walker <spacedoesrs@gmail.com>
2024-09-05 18:40:40 +0200

    fix custom payload id conflict with rsmm

--- src/main/java/block/event/separator/network/PayloadWrapper.java
+++ src/main/java/block/event/separator/network/PayloadWrapper.java
@@ -1,13 +1,16 @@
 package block.event.separator.network;
 
+import block.event.separator.BlockEventSeparatorMod;
+
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
 
 public record PayloadWrapper(Payload payload) implements CustomPacketPayload {
 
 	public static final StreamCodec<FriendlyByteBuf, PayloadWrapper> STREAM_CODEC = CustomPacketPayload.codec(Payloads::encode, Payloads::decode);
-	public static final Type<PayloadWrapper> TYPE = CustomPacketPayload.createType("rsmm");
+	public static final Type<PayloadWrapper> TYPE = new CustomPacketPayload.Type<>(new ResourceLocation(BlockEventSeparatorMod.MOD_ID, "payloads"));
 
 	public PayloadWrapper(Payload payload) {
 		this.payload = payload;
