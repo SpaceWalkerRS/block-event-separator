4b2db2707f5d6ff91306866764a79e4cb4e49b9f
Space Walker <spacedoesrs@gmail.com>
2025-03-01 14:48:08 +0100

    bump version to 1.3.2

--- src/main/java/block/event/separator/BlockEventSeparatorMod.java
+++ src/main/java/block/event/separator/BlockEventSeparatorMod.java
@@ -16,7 +16,7 @@ public class BlockEventSeparatorMod implements ModInitializer {
 
 	public static final String MOD_ID = "block-event-separator";
 	public static final String MOD_NAME = "Block Event Separator";
-	public static final String MOD_VERSION = "1.3.1";
+	public static final String MOD_VERSION = "1.3.2";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
 	private static final List<Runnable> SERVER_SEPARATION_MODE_LISTENERS = new LinkedList<>();
